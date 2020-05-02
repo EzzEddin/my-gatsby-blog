8d71c5f5cddd4f17bec936d813f773bbc181df7d
ezzeddin <ezzeddinabdullah@gmail.com>
2020-05-02 15:46:27 +0200

    fix contact form to be submitted to netlify

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -27,20 +27,20 @@ const ContactPage = ({
           data-netlify-honeypot="bot-field"
         >
             <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+              <label>Name</label>
+              <input type="text" name="name"/>
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
+              <label>Email</label>
+              <input type="email" name="email"/>
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
+              <label>Subject</label>
+              <input type="text" name="subject"/>
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <label>Message</label>
+              <textarea name="message"></textarea>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
               <input type="submit" className="button -primary" style={{marginRight: 0}} />
