817c7ef47171bd0810571690bab1bfdc664c704d
ezzeddin <ezzeddinabdullah@gmail.com>
2020-05-02 17:30:27 +0200

    update style to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,9 @@
-import React from "react"
-import Helmet from "react-helmet"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = ({ data: { site } }) => {
   return (
     <Layout>
       <Helmet>
@@ -15,34 +11,52 @@ const ContactPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+        <div
+          className="post-thumbnail"
+          style={{
+            backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`,
+            marginBottom: 0
+          }}
+        >
           <h1 className="post-title">Get in Touch</h1>
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-              <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>   
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <input
+                type="submit"
+                className="button -primary"
+                style={{ marginRight: 0 }}
+              />
+            </div>
+          </form>
         </div>
       </div>
     </Layout>
-  )
-}
-export default ContactPage
+  );
+};
+export default ContactPage;
 export const pageQuery = graphql`
-  query ContactPageQuery{
+  query ContactPageQuery {
     site {
       siteMetadata {
         title
@@ -50,4 +64,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
