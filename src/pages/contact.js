fc39eaccbbd02a3f698cd034c0d6a5ebdc58c403
ezzeddin <ezzeddinabdullah@gmail.com>
2020-05-02 15:39:48 +0200

    add contact form to netlify

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,12 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
+        <form
+          name="contact-form"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
             <div>
               <label htmlFor="w3lName">Name</label>
               <input type="text" name="w3lName" id="w3lName"/>
