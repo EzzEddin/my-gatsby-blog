038e21d9ec974770235f8f49eb314200bd6d6671
ezzeddin <ezzeddinabdullah@gmail.com>
2020-05-14 23:59:21 +0200

    update linkedin icon location

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import ThemeChanger from "../components/themeChanger";
-import Linkedin from "./SocialLinks/linkedin.svg";
+import Linkedin from "./linkedin.svg";
 
 export default props => (
   <nav className="navigation">
