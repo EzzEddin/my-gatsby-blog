d0f4f1cd87aac26a496e8aabde76aa59f1600cd2
ezzeddin <ezzeddinabdullah@gmail.com>
2020-05-14 23:52:11 +0200

    add Linkedin icon

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
 import ThemeChanger from "../components/themeChanger";
-import Linkedin from './SocialLinks/linkedin.svg'
+import Linkedin from "./SocialLinks/linkedin.svg";
 
 export default props => (
   <nav className="navigation">
-    <Link
-      href="https://linkedin.com/in/ezzeddinabdullah"
-      title="Linkedin"
-      className="linkedin"
-    >
-      <Linkedin />
-    </Link>
+    <Linkedin />
     <ThemeChanger />
   </nav>
 );
