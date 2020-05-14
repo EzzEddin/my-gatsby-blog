6116f73ccebc9cc9bc51b3653efcaf6ee7b5f354
ezzeddin <ezzeddinabdullah@gmail.com>
2020-05-14 23:46:16 +0200

    add link in navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,17 @@
-import React from "react"
-import {Link} from "gatsby"
-import ThemeChanger from "../components/themeChanger"
+import React from "react";
+import { Link } from "gatsby";
+import ThemeChanger from "../components/themeChanger";
+import Linkedin from './SocialLinks/linkedin.svg'
 
-export default (props) => (
-  <nav className="navigation"> 
-    <ThemeChanger/>
+export default props => (
+  <nav className="navigation">
+    <Link
+      href="https://linkedin.com/in/ezzeddinabdullah"
+      title="Linkedin"
+      className="linkedin"
+    >
+      <Linkedin />
+    </Link>
+    <ThemeChanger />
   </nav>
-  
-)
+);
